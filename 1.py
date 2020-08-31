fa9f9633bdf58a4e28d6f1bd6b6d23c78c1a6b47
DenisRomantsev <67921196+DenisRomantsev@users.noreply.github.com>
2020-09-01 02:01:10 +0700

    hw
    hw

---
+++ 1.py
@@ -0,0 +1,10 @@
+# первое задание
+
+
+
+my_list = [False, None, -77, 'True', 1488, 2.28]
+def my_type(el):
+    for el in range(len(my_list)):
+        print(type(my_list[el]))
+    return
+my_type(my_list)
