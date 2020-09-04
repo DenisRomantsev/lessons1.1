3c1bbea94d39c36bbe05cd3dc5bf5c81a2f1a01a
DenisRomantsev <67921196+DenisRomantsev@users.noreply.github.com>
2020-09-04 18:40:50 +0700

    hw
    new hw

---
+++ 2.py
@@ -0,0 +1,12 @@
+   # второе задание
+
+   name = input('enter name')
+   surname = input('enter surname')
+   year = int(input('enter year'))
+   city = input('enter city')
+   email = input('enter email')
+   telephone = input('input telephone')
+   '''
+   def my_func (name, surname, year, city, email, telephone):
+        return ' '.join([name, surname, year, city, email, telephone])
+   print(my_func(surname = 'Romantsev', name = 'Denis', year = '1991', city = 'Novosibirsk', email = 'mail.silvan@gmail.com', telephone = '905-905-1488'))
